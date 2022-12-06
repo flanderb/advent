e7873cd6f7b318e701123f3a167d0e163f8d0686
Benjamin Flanders <bflanders@fox-sales.com>
2022-12-06 16:42:55 -0500

    missing q between c and s

--- day6.rb
+++ day6.rb
@@ -1,36 +1,40 @@
 class Datastream
     def initialize(input)
-        @contained_pairs = 0
-        input_file(input)
+        @data_stream = input_file(input)
+        @marker_size = 14
+        @first_marker = find_first_marker(@data_stream)
+
     end
 
-    attr_reader :contained_pairs
+    attr_reader :data_stream, :first_marker
 
     def input_file(input)
+        File.open(input).read
+    end
 
-        File.foreach(input) do |line|
-            one_tasks = get_tasks(line, 0)
-            two_tasks = get_tasks(line, 1)
-            if contained_pair?(one_tasks, two_tasks)
-                @contained_pairs += 1
+    def find_first_marker(stream)
+        x = @marker_size +1
+        stream.each_char.with_index(x) do |char, i|
+            p i
+            # p "stream[#{i}..#{i + 4}]"
+            temp_array = stream[i-x..i].chars.uniq
+            p temp_array
+            p temp_array.size
+            if temp_array.size == x
+                x = i
+                break
             end
         end
+        return x+1
     end
 
-    def get_tasks(line, elf)
-        elfs_tasks_string = line.split(",")[elf]
-        elfs_tasks = elfs_tasks_string.split("-").map{|a| a.to_i}.to_a
-    end
 
-    def contained_pair?(elf1, elf2)
-        r1 = Range.new(elf1[0], elf1[1], false)
-        r2 = Range.new(elf2[0], elf2[1], false)
-        return ((r1.to_a & r2.to_a) == r1.to_a) || ((r2.to_a & r1.to_a) == r2.to_a)
-    end
+
+
 
 
 end
 
 
-today = Jobs.new("day4i")
-puts today.contained_pairs
+today = Datastream.new("day6i.txt")
+p today.first_marker
--- test.rb
+++ test.rb
@@ -1,20 +1,22 @@
-test = Array.new(2){Array.new}
-test[0] = ['A', 'B', 'C']
-test[1] = ['D', 'E', 'F']
-
-
-def make_move(stacks, number, from, to)
-
-    stacks[to].push(stacks[from].pop(number)).flatten!
-
+def find_first_marker(stream)
+    x = 14 
+    stream.each_char.with_index(x) do |char, i|
+        p i
+        # p "stream[#{i}..#{i + 4}]"
+        temp_array = stream[i-x..i].chars.uniq
+        p temp_array
+        p temp_array.size
+        if temp_array.size == x 
+            x = i
+            break
+        end
+    end
+    return x + 1
 end
 
-test.each do |item|
-    p item
-end
+test = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+test1 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+test2 = "nppdvjthqldpwncqszvftbrmjlhg" #missing q between c and s why
+test3 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
 
-make_move(test, 2, 1, 0)
-
-test.each do |item|
-    p item
-end
+p find_first_marker(test2)
