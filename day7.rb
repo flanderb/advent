d28b7c7cab4854e3a7dd84ac9002915a0922f2d2
Benjamin Flanders <flanderb@gmail.com>
2022-12-08 07:22:40 -0500

    Day 7 done

--- day7.rb
+++ day7.rb
@@ -1,30 +1,25 @@
-class Node
-    attr_accessor :children, :value
-        
-    def initialize(v)
-        @value = v
-        @children = []
-
-    end
-end
-
 class Filesystem
+
+    attr_reader :dir_size, :dir_tree, :total_small_dirs
+
     def initialize(input)
-        @system = populate(input)
-        @dir_size = = {'/': 0} 
+        
+        @dir_size = Hash.new
         @dir_tree = Array.new
+        populate(input)
+        @total_small_dirs = add_small_directories(@dir_size)
 
     end
-
-    attr_reader :system, :dir_size, :dir_tree
 
     def populate(input)
         File.foreach(input) do |line|
-            case line.gsub(/[$ ]/, '')[0]
+            line = line.gsub("\n", "").gsub("$ ", "")
+            # p line
+            case line[0]
             when 'c'
                 #cd this is a change directory command update dir_tree
-                update_dir_tree(line.gsub(/[cd ]/, ''))
-            when match(/[0-9]/)
+                update_dir_tree(line.gsub("cd ", ''))
+            when /\d/
                 #this is a file add to current dir_tree entries
                 update_dir_size(line.gsub(/\D/, ''))
             when 'd'
@@ -32,28 +27,75 @@ class Filesystem
             when 'l'
                 # ls ignore
             end
-    end
-
-    def update_dir_tree(directory)
-        case directory
-        when "/"
-            @dir_tree.clear
-            @dir_tree.push('/')
-        when ".."
-            @dirtree.pop
-        else
-            @dir_tree.push directory
-            @dir_size[diectory] = 0
         end
     end
 
+    def update_dir_tree(dir)
+        case dir
+        when "/"
+            @dir_tree.clear
+            @dir_tree.push('/')
+            @dir_size[dir] =  0
+        when ".."
+            @dir_tree.pop
+        else
+            temp = @dir_tree.join << dir
+            @dir_tree.push temp
+            @dir_size[temp] = 0
+        end
+        # p @dir_tree
+    end
+
     def update_dir_size(line)
-        @dir_size.each do 
+ 
+        @dir_tree.each do |dir|
+            @dir_size[dir] += line.to_i
+        end
+        # p @dir_size
+    end
+
+    def add_small_directories(all_dir)
+        total = 0
+        all_dir.each do |dir, size|
+            if size <= 100000
+                # p dir
+                total += size
+            end
+        end
+        return total
+    end
+
+    def free_up_space
+        space_from_dir = 0
+        disk_size = 70000000
+        update_needs = 30000000
+        used = @dir_size["/"]
+        unused_space = disk_size - used
+        p unused_space
+        need_free_up = update_needs - unused_space
+        p need_free_up
+        
+        size_array = @dir_size.values
+        p size_array
+        size_array.sort!
+        p size_array
+
+        size_array.each do |size|
+            if size > need_free_up
+                space_from_dir = size
+                break
+            end
+        end
+        return space_from_dir
+
     end
 
 
 end
 
-
-today = Datastream.new("day6i.txt")
-p today.first_marker
+# files = Filesystem.new("testi.txt")
+files = Filesystem.new("day7i.txt")
+# p files.dir_size
+p files.total_small_dirs
+p "--_--_--_--_--_-__-__-__-_--_"
+p files.free_up_space
--- test.rb
+++ test.rb
@@ -1,18 +1,45 @@
-test = {'/': 0} 
+# test = {'/': 0} 
 
-test['this'] = 0
+# test['this'] = 0
 
-p test
+# p test
 
+# puts "-_--_--_--__-__-__-__-__"
 
-test.each do |dir, size|
-    p "#{dir} -> #{size} "
+# test.each do |dir, size|
+#     p "#{dir} -> #{size} "
+# end
+# puts "-_--------__-__-_--_-__-"
+# test.each do |dir, size|
+#     test[dir] += 45
+# end
+
+# test.each do |dir, size|
+#     p "#{dir} -> #{size} "
+# end
+
+input_file = "day7i.txt"
+
+def populate(input)
+    File.foreach(input) do |line|
+        temp =  line.gsub(/[$ ]/, '')[0]
+        case temp
+        when 'c'
+            #cd this is a change directory command update dir_tree
+            # self.update_dir_tree(line.gsub(/[cd ]/, ''))
+            p "cd #{temp}-#{line}"
+        when /\d/
+            #this is a file add to current dir_tree entries
+            # update_dir_size(line.gsub(/\D/, ''))
+            p "file #{temp}-#{line}"
+        when 'd'
+            #dir this is a directory Ignore
+            p "dir#{temp}-#{line}"
+        when 'l'
+            # ls ignore
+            p "ls #{temp}-#{line}"
+        end
+    end
 end
 
-test.each do |dir, size|
-    test[dir].size += 45
-end
-
-test.each do |dir, size|
-    p "#{dir} -> #{size} "
-end
+p populate(input_file) 
