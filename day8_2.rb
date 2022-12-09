e8c63904c0ab5e29635614fa8a3e6a5a78dc1058
Benjamin Flanders <bflanders@fox-sales.com>
2022-12-09 09:55:12 -0500

    day9_2 done

--- day8_2.rb
+++ day8_2.rb
@@ -1,10 +1,10 @@
 class Forest
     def initialize(input)
         @forest = input_file(input)
-        @viewable_trees = find_viewable_trees(@forest)
+        @best_view = find_best_view(@forest)
     end
 
-    attr_reader :viewable_trees
+    attr_reader :best_view
 
     def input_file(input)
         forest = Array.new
@@ -22,7 +22,7 @@ class Forest
 
         forest.each_index do |row|
             forest[row].each_index do |column|
-                puts "[#{row}, #{column}] = #{forest[row][column]}"
+                # puts "[#{row}, #{column}] = #{forest[row][column]}"
                 view = view_from_tree(forest, row, column)
                 # puts viewable
                 if view > best_view
@@ -39,11 +39,11 @@ class Forest
         north = view_to_north?(forest, row, column)
         south = view_to_south?(forest, row, column)
 
-        p "[#{row},#{column}}] north = #{north}"
-        p "[#{row},#{column}}] south = #{south}"
-        p "[#{row},#{column}}] east = #{east}"
-        p "[#{row},#{column}}] west = #{west}"
-        p "#{east * west * north * south}"
+        # p "[#{row},#{column}] north = #{north}"
+        # p "[#{row},#{column}] south = #{south}"
+        # p "[#{row},#{column}] east = #{east}"
+        # p "[#{row},#{column}] west = #{west}"
+        # p "#{east * west * north * south}"
 
         
         return east * west * north * south
@@ -53,77 +53,70 @@ class Forest
     def view_to_east?(forest, row, column)
         #get height of tree
         height = forest[row][column]
-        height_to_east = -1
-        p "[#{row}][#{column}] east -_---_-_-_-_-_-_-_"
-        for a in 0..column-1
-            p "forest[#{row}][#{a}] = #{forest[row][a]}"
-            if forest[row][a] > height_to_east #&& column != a
-                height_to_east = forest[row][a]
+        range = 0..column - 1
+        view_count = 0
+        range.reverse_each do |col|
+            if forest[row][col] >= height
+                view_count += 1
+                break
+            else
+                view_count += 1
             end
         end
-        # p "forest[#{x}][#{y}]"
-        p height
-        p height_to_east
-        p height > height_to_east
-        return height > height_to_east
+        return view_count
     end
 
     def view_to_west?(forest, row, column)
         #get height of tree
         height = forest[row][column]
-        height_to_west = -1
-        p "[#{row}][#{column}] west -_---_-_-_-_-_-_-_"
-        for a in column+1..forest[0].length-1
-            p "forest[#{row}][#{a}] = #{forest[row][a]}"
-            if forest[row][a] > height_to_west #&& a != forest[0].length
-                height_to_west = forest[row][a]
+        range = (column + 1)..(forest[row].length - 1)
+        view_count = 0
+        range.each do |col|
+            if forest[row][col] >= height
+                view_count += 1
+                break
+            else
+                view_count += 1
             end
         end
-        # p "forest[#{x}][#{y}]"
-        p height
-        p height_to_west
-        p height > height_to_west
-        return height > height_to_west
+        return view_count
     end
 
     def view_to_north?(forest, row, column)
         #get height of tree
         height = forest[row][column]
-        height_to_north = -1
-        p "[#{row}][#{column}] north -_---_-_-_-_-_-_-_"
-        for a in 0..row-1
-            p "forest[#{a}][#{column}] = #{forest[a][column]}"
-            if forest[a][column] > height_to_north #&& a != forest.length
-                height_to_north = forest[a][column]
+        range = 0..(row - 1)
+        view_count = 0
+        range.reverse_each do |row|
+            if forest[row][column] >= height
+                view_count += 1
+                break
+            else
+                view_count += 1
             end
         end
-        # p "forest[#{x}][#{y}]"
-        p height
-        p height_to_north
-        p height > height_to_north
-        return height > height_to_north
+        return view_count
     end
-end
 
-def view_to_south?(forest, row, column)
-    #get height of tree
-    height = forest[row][column]
-    height_to_south = -1
-    p "[#{row}][#{column}] south -_---_-_-_-_-_-_-_"
-    for a in row+1..forest.length-1
-        p "forest[#{a}][#{column}] = #{forest[a][column]}"
-        if forest[a][column] > height_to_south #&& a != forest.length
-            height_to_south = forest[a][column]
+
+    def view_to_south?(forest, row, column)
+        #get height of tree
+        height = forest[row][column]
+        range = (row + 1)..(forest.length - 1)
+        view_count = 0
+        range.each do |row|
+            if forest[row][column] >= height
+                view_count += 1
+                break
+            else
+                view_count += 1
+            end
         end
+        return view_count 
     end
-    # p "forest[#{x}][#{y}]"
-    p height
-    p height_to_south
-    p height > height_to_south
-    return height > height_to_south
+
 end
 
+today = Forest.new("day8i.txt")
+p today.best_view
 
-today = Forest.new("testi.txt")
-p today.viewable_trees
-p today.viewable_trees.length
