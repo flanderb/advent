e44d314066fe91b415c29fdf51a4d0ea84479d5a
Benjamin Flanders <bflanders@fox-sales.com>
2022-12-09 16:42:50 -0500

    working on Day 9

---
+++ day9.rb
@@ -0,0 +1,66 @@
+class string
+    def initialize(input)
+        @visited_positions = Array.new
+
+        input_file(input)
+    end
+
+    attr_reader :visited_positions
+
+    def input_file(input)
+        visited_positions = Array.new
+        head = [0,0]
+        tail = [0,0]
+
+        File.foreach(input) do |line|
+            direction, distance = line.split
+            distance.times do |direction|
+                head = move_head(head, direction)
+                tail = move_tail(head, tail, direction)
+                visited_positions.push(tail.join)
+            end
+        end
+    end
+
+    def move_head(head, dir)
+        case dir
+        when "U"
+            head[1] += 1
+        when "D"
+            head[1] -= 1
+        when "L"
+            head[0] -= 1
+        when "R"
+            head[0] += 1
+        end
+        return head
+    end
+
+    def move_tail(head, tail, dir)
+        if !is_tail_next_to_head(head, tail)
+            case dir
+            when "U"
+                tail[1] += 1
+            when "D"
+                tail[1] -= 1
+            when "L"
+                tail[0] -= 1
+            when "R"
+                tail[0] += 1
+            end
+            return tail
+        end
+    end
+
+    def is_tail_next_to_head(head, tail)
+        x_diff = head[0] - tail[0]
+        y_diff = head[1] - tail[1]
+        return x_diff > 1 || y_diff > 1
+    end
+
+
+end
+
+
+today = Jobs.new("testi.txt")
+puts today.visited_positions
--- test.rb
+++ test.rb
@@ -1,19 +1,50 @@
-def input_file(input)
-    forest = Array.new
-    i = 0
-    File.foreach(input) do |row|
-        row = row.gsub("\n", "")
-        forest.push(row.split(//).map{|char| char.to_i})
-        i += 1
+def move_tail(head, tail)
+    x_diff = head[0] - tail[0]
+    y_diff = head[1] - tail[1]
+
+    if xdiff.abs == 2 || y_diff.abs == 2
+        #move 1 in x 
+        if head[0] > tail[0]
+            tail[0] += 1
+        elsif head[0] < tail[0]
+            tail[0] -= 1
+        end
+                
+        #and 1 in y
+        if head[1] > tail[1]
+            tail[1] += 1
+        elsif head[1] < tail[1]
+            tail[1] -= 1
+        end
     end
-    return forest
+
+    case dir
+    when "U"
+        tail[1] += 1
+    when "D"
+        tail[1] -= 1
+    when "L"
+        tail[0] -= 1
+    when "R"
+        tail[0] += 1
+    end
+    return tail
+
 end
 
-test = input_file('testi.txt')
+t1 = [0,0]
+h1 = [0,0]
+h2 = [0,1]
+h3 = [1,0]
+h4 = [1,1]
+h5 = [0,2]
+h6 = [1,1]
 
-p test
-p test[2][3]
 
-x, y = 1
-p x
-p y
+
+# head = h1
+tail = t1
+head_moves = [h1, h2, h3, h4, h5, h6]
+head_moves.each do |head|
+    p move_tail(head, tail)
+end
