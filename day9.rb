08005eb5fe908587941c57741973b651979b6849
Benjamin Flanders <flanderb@gmail.com>
2022-12-10 22:15:54 -0500

    Day 9 complete.  Both challenges in one file

--- day9.rb
+++ day9.rb
@@ -1,66 +1,96 @@
-class string
+class Rope
     def initialize(input)
-        @visited_positions = Array.new
+        @visited_positions = input_file(input)
+        @num_tail_visited = @visited_positions.uniq.length
 
         input_file(input)
     end
 
-    attr_reader :visited_positions
+    attr_reader :visited_positions, :num_tail_visited
 
     def input_file(input)
         visited_positions = Array.new
-        head = [0,0]
-        tail = [0,0]
+        rope_size = 10
+        rope = Array.new(rope_size){[0,0]}
 
         File.foreach(input) do |line|
+            p "--_--_--_--_--__-__--_--_--_--__-__-__-__--_--_-__--__--_--__--_--__--_--__--_---_--__-_-__--_--_--__"
             direction, distance = line.split
-            distance.times do |direction|
-                head = move_head(head, direction)
-                tail = move_tail(head, tail)
-                visited_positions.push(tail.join)
+            distance = distance.to_i
+            # p "#{direction} - #{distance}"
+            distance.times do |iteration|
+                p "==+==+==+==+==+==++==+==+===++"
+                p "Distance = #{distance}, iteration = #{iteration} direction = #{direction}"
+                rope = move_rope(rope, direction)
+                # p "~~~~~~~~~~~~~~~~~~~~~~~"
+                p rope
+                # p "~~~~~~~~~~~~~~~~~~~~~~~"
+                visited_positions.push(rope.last.join(","))
+            end
+        end
+        return visited_positions
+    end
+
+    def move_rope(rope, dir)
+        # p "dir = #{dir}"
+        rope.each_index do |index|
+            # p "******************"
+            # p "knot = #{index}"
+            # p "rope_b = #{rope}"
+            # p "rope[#{index}] = #{rope[index]} "
+            # p "rope[#{index - 1}] = #{rope[index - 1]} "
+            if index == 0
+                case dir
+                when "U"
+                    rope[index][1] += 1
+                when "D"
+                    rope[index][1] -= 1
+                when "L"
+                    rope[index][0] -= 1
+                when "R"
+                    rope[index][0] += 1
+                end
+            else
+                # p "leader = #{rope[index - 1]}"
+                leader = rope[index - 1]
+                # p "follower = #{rope[index]}"
+                follower = rope[index]
+
+                x_diff = leader[0] - follower[0]
+                y_diff = leader[1] - follower[1]
+                p "leader = #{leader}; follower = #{follower}"
+            
+                if x_diff.abs == 2 && y_diff.abs == 1
+                    follower[0] += x_diff/2
+                    follower[1] += y_diff 
+                elsif y_diff.abs == 2 && x_diff.abs == 1
+                    follower[0] += x_diff 
+                    follower[1] += y_diff/2                
+                elsif y_diff.abs == 2 && x_diff.abs == 2
+                    follower[0] += x_diff/2
+                    follower[1] += y_diff/2
+                elsif x_diff.abs == 2 && y_diff == 0
+                    follower[0] += x_diff/2
+                elsif y_diff.abs == 2 && x_diff == 0
+                    follower[1] += y_diff/2
+                end
             end
         end
     end
-
-    def move_head(head, dir)
-        case dir
-        when "U"
-            head[1] += 1
-        when "D"
-            head[1] -= 1
-        when "L"
-            head[0] -= 1
-        when "R"
-            head[0] += 1
-        end
-        return head
-    end
-
-    def move_tail(head, tail)
-        if !is_tail_next_to_head(head, tail)
-            case dir
-            when "U"
-                tail[1] += 1
-            when "D"
-                tail[1] -= 1
-            when "L"
-                tail[0] -= 1
-            when "R"
-                tail[0] += 1
-            end
-            return tail
-        end
-    end
-
-    def is_tail_next_to_head(head, tail)
-        x_diff = head[0] - tail[0]
-        y_diff = head[1] - tail[1]
-        return x_diff <= 1 && y_diff <= 1
-    end
-
-
 end
+# 
 
+# today = Rope.new("day9i.txt")
+today = Rope.new("day9i.txt")
 
-today = Jobs.new("testi.txt")
-puts today.visited_positions
+p today.visited_positions
+p today.visited_positions.uniq
+
+puts today.num_tail_visited
+
+#The 1,2 is wrong
+# "Distance = 8, iteration = 2 direction = U"
+#   24   │ [[5, 3], [5, 2], [4, 1], [3, 1], [2, 1], [1, 1], [0, 0], [0, 0], [0, 0], [0, 0]]
+#   25   │ "==+==+==+==+==+==++==+==+==+"
+#   26   │ "Distance = 8, iteration = 3 direction = U"
+#   27   │ [[5, 4], [5, 3], [5, 2], [4, 2], [3, 2], [2, 2], [1, 2], [1, 1], [0, 0], [0, 0]]
--- test.rb
+++ test.rb
@@ -1,50 +1,26 @@
-def move_tail(head, tail)
-    x_diff = head[0] - tail[0]
-    y_diff = head[1] - tail[1]
 
-    if xdiff.abs == 2 || y_diff.abs == 2
-        #move 1 in x 
-        if head[0] > tail[0]
-            tail[0] += 1
-        elsif head[0] < tail[0]
-            tail[0] -= 1
-        end
-                
-        #and 1 in y
-        if head[1] > tail[1]
-            tail[1] += 1
-        elsif head[1] < tail[1]
-            tail[1] -= 1
-        end
+rope = Array.new(9) { |i| [i,i] }
+
+head = rope.first
+
+# p rope
+# p head
+
+rope_size = 2
+rope = Array.new(rope_size){[0,0]}
+# rope = [[0,0],[0,0]]
+
+rope.each_index do |index|
+    p index
+    if index == 0
+        p "index is zero"
+        rope[index][0] = 5
+        p rope[index]
+        p rope
+    else
+        p rope[index]
     end
 
-    case dir
-    when "U"
-        tail[1] += 1
-    when "D"
-        tail[1] -= 1
-    when "L"
-        tail[0] -= 1
-    when "R"
-        tail[0] += 1
-    end
-    return tail
-
 end
+p rope
 
-t1 = [0,0]
-h1 = [0,0]
-h2 = [0,1]
-h3 = [1,0]
-h4 = [1,1]
-h5 = [0,2]
-h6 = [1,1]
-
-
-
-# head = h1
-tail = t1
-head_moves = [h1, h2, h3, h4, h5, h6]
-head_moves.each do |head|
-    p move_tail(head, tail)
-end
